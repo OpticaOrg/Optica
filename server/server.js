c2498b8e7e6cc691009c8004e8991077f04db79c
Austin Hoang <austinhoang14@gmail.com>
2023-03-11 13:30:26 -0500

    image controller and SQL image model created

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
+const imageController = require('./controller/imageController');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.get('/images', imageController.getImageFromSQL, (req, res) => {
+  return res.status(200).send('Image retrieved from database!');
+});
+
+app.post('/images', imageController.saveImageToSQL, (req, res) => {
+  return res.status(200).send('Image saved to database!');
+})
 
 // catch-all route handler for any requests to an unknown route
 app.use((req, res) =>
@@ -17,7 +26,7 @@ app.use((req, res) =>
  */
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).send({ error: err });
+  res.status(500).send('Something broke!');
 });
 
 app.listen(PORT, () => {
