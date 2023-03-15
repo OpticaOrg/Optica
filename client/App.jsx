71ff5df2551f22974a15eaa025bda2519800ddd8
Yomkool <bannon.pj@gmail.com>
2023-03-15 13:22:02 -0400

    added react router

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,16 @@
 import './stylesheets/styles.scss'
 import React from 'react'
 import { MainPage } from './features/mainPage/mainPageContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './features/loginPage/Login'
 
 export default function App() {
   return (
-    <MainPage />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
