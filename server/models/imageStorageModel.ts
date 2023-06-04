b323150e4cd297ae84ad99ec87fb505306102e37
Adam Liang <me@adamliang.dev>
2023-06-04 17:16:35 -0400

    - converted aws sdk v2 to v3 - changed to async await - added all types... no errors!

--- server/models/imageStorageModel.ts
+++ server/models/imageStorageModel.ts
@@ -1,4 +1,5 @@
-import AWS from 'aws-sdk';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import dotenv from 'dotenv';
 import { v4 as uuidv4 } from 'uuid';
 import { z } from 'zod';
@@ -14,11 +15,6 @@ const envVarsSchema = z.object({
 
 const { AWS_S3_ACCESS_KEY_ID, AWS_S3_SECRET_ACCESS_KEY, BUCKET_NAME, AWS_URL_STEM } = envVarsSchema.parse(process.env);
 
-// Create a connection to the AWS S3 server.
-const s3 = new AWS.S3({
-  accessKeyId: AWS_S3_ACCESS_KEY_ID,
-  secretAccessKey: AWS_S3_SECRET_ACCESS_KEY
-});
 
 // Uplaod the image blob to a specified S3 bucket.
 const uploadFile = async (blob : string) => {
@@ -30,25 +26,38 @@ const uploadFile = async (blob : string) => {
   // } catch (e) {
   //   throw e;
   // }
+
+
+  // Setting up client connection to S3.
+  const client = new S3Client({ 
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: AWS_S3_ACCESS_KEY_ID,
+      secretAccessKey: AWS_S3_SECRET_ACCESS_KEY
+    },
+    endpoint: AWS_URL_STEM,
+  });
+
   // Setting up S3 upload parameters
-  const params = {
-    Bucket: process.env.BUCKET_NAME,
-    Key: `${uuidv4()}.jpg`, // File name you want to save as in S3 -- maybe a randomly generated key.
+  const params = new PutObjectCommand({
+    Bucket: BUCKET_NAME,
+    // File name you want to save as in S3 -- maybe a randomly generated key.
+    Key: `${uuidv4()}.jpg`,
+    // hehe blobs 
     Body: blob
-  };
+  });
 
   // Uploading files to the bucket. Return the URL. See https://stackoverflow.com/questions/57420576/how-to-synchronously-upload-files-to-s3-using-aws-sdk.
-  let awsReturnedURL;
-  // Will need to catch errors here and pass to global error handler.
-  const stored = await s3
-    .upload(params, function (err, data) {
-      if (err) {
-        throw err;
-      }
-    })
-    .promise();
-  awsReturnedURL = process.env.AWS_URL_STEM + stored.Key;
-  return awsReturnedURL;
+
+  try {
+    const result = await client.send(params);
+    return await getSignedUrl(client, params, {
+      expiresIn: 3600
+    });
+  }
+  catch (e) {
+    throw e;
+  }
 };
 
 export default uploadFile;
