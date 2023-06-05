ac78a7649fe0574f0bce8007487f829174c8628f
Adam Liang <me@adamliang.dev>
2023-06-04 22:25:35 -0400

    moved slices and store to redux folder for better organization

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import store from './store/store';
+import store from './redux/store';
 
 // wrapping App in a Provider component with store prop makes the store available to the App
 const container = document.querySelector('#root') as HTMLElement;
--- src/redux/slices/counterSlice.ts
+++ src/redux/slices/counterSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, type PayloadAction, type ThunkAction } from '@reduxjs/toolkit';
-import { type RootState } from '../../store/store';
+import { type RootState } from '../store';
 
 interface CounterState {
   value: number
